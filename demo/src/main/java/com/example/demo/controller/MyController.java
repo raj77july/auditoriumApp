c1654426432541edd6ce8e94c4617bb275e2d1fb
raj77july <raj77july@gmail.com>
2023-11-01 20:02:06 +0530

    #newtest9

--- demo/src/main/java/com/example/demo/controller/MyController.java
+++ demo/src/main/java/com/example/demo/controller/MyController.java
@@ -50,7 +50,7 @@ public class MyController {
 	@GetMapping("/test")
 	public String displayAllShow1()
 	{
-		return "ShrsdseerasdRassssmShree Ram Jai Ram";
+		return "ShrsdseerasdRassssmShree9 Ram Jai Ram";
 	}
 	
 
